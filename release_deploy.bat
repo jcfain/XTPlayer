f11b39ff2d7788a1c96bde2895b9cea61f580de3
jcfain <jfain878@gmail.com>
2020-08-21 14:46:50 -0400

    Renaming the project. Adding a windows deploy batch

---
+++ release_deploy.bat
@@ -0,0 +1,3 @@
+windeployqt .\build-XTPlayer-Desktop_Qt_5_15_0_MinGW_64_bit-Release\release\XTPlayer.exe -dir .\bin\release\
+xcopy .\build-XTPlayer-Desktop_Qt_5_15_0_MinGW_64_bit-Release\release\XTPlayer.exe .\bin\release\
+pause
