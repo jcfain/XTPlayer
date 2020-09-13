ad4977ecd789d094b70f97e2f1ae593c6b71fc06
Jason C. Fain <jfain878@gmail.com>
2020-09-13 16:52:54 -0400

    Adding linux deploy script

---
+++ lin_release_deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cqtdeployer -bin ./build-XTPlayer-Desktop_Qt_5_15_0_GCC_64bit-Release/ -targetDir ./XTPlayer-release 
+#-deployVersion 0.13b -releaseDate $(date) +'%m/%d/%Y' -icon ./src/images/icons/XTP-icon.png
+cp -R src/themes ./XTPlayer-release/bin
+read -p "Press enter to resume ..."
+
+
