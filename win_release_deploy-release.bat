064595a4d5b97d350a4115f3fb47004858e26372
Jason Fain <jfain878@gmail.com>
2021-03-21 09:41:01 -0400

    Improving the deploy batch

--- win_release_deploy-release.bat
+++ win_release_deploy-release.bat
@@ -1,15 +1,31 @@
-C:\Qt\5.15.0\mingw81_64\bin\windeployqt .\build-XTPlayer-Desktop_Qt_5_15_0_MinGW_64_bit-Release\release\XTPlayer.exe -dir .\bin\release\
-xcopy .\build-XTPlayer-Desktop_Qt_5_15_0_MinGW_64_bit-Release\release\XTPlayer.exe .\bin\release\
-xcopy .\build-XTPlayer-Desktop_Qt_5_15_0_MinGW_64_bit-Release\release\themes .\bin\release\themes /s /i
-xcopy .\build-QtAV-Desktop_Qt_5_15_0_MinGW_64_bit-Release\bin\*.dll .\bin\release\
-xcopy .\libav-x86_64-w64-mingw32-11.7\usr\bin\*.dll .\bin\release\
-xcopy C:\Qt\5.15.0\mingw81_64\bin\libgcc_s_seh-1.dll .\bin\release\
-xcopy C:\Qt\5.15.0\mingw81_64\bin\libstdc++-6.dll .\bin\release\
-xcopy C:\Qt\5.15.0\mingw81_64\bin\libwinpthread-1.dll .\bin\release\
-xcopy C:\Qt\5.15.0\mingw81_64\bin\Qt5OpenGL.dll .\bin\release\
-xcopy C:\Qt\5.15.0\mingw81_64\bin\Qt5Compress.dll .\bin\release\
-xcopy .\src\rename_me_for_local_settings.ini .\bin\release\
-xcopy .\src\XTPlayerDebug.bat .\bin\release\
-xcopy .\src\README.txt .\bin\release\
+SET QtDir=C:\Qt\5.15.2\mingw81_64\bin\
+SET buildDir=.\build-XTPlayer-Desktop_Qt_5_15_2_MinGW_64_bit-Release\
+SET deployDir=.\bin\release\
+
+%QtDir%windeployqt %buildDir%release\XTPlayer.exe -dir %deployDir%
+xcopy %buildDir%release\XTPlayer.exe %deployDir%
+xcopy %buildDir%release\themes %deployDir%themes /s /i
+xcopy %QtDir%QtAV1.dll %deployDir%
+xcopy %QtDir%QtAVWidgets1.dll %deployDir%
+xcopy %QtDir%avcodec-56.dll %deployDir%
+xcopy %QtDir%avdevice-55.dll %deployDir%
+xcopy %QtDir%avfilter-5.dll %deployDir%
+xcopy %QtDir%avformat-56.dll %deployDir%
+xcopy %QtDir%avresample-2.dll %deployDir%
+xcopy %QtDir%avutil-54.dll %deployDir%
+xcopy %QtDir%swscale-3.dll %deployDir%
+xcopy %QtDir%zlib1.dll %deployDir%
+xcopy %QtDir%libvo-aacenc-0.dll %deployDir%
+xcopy %QtDir%libmp3lame-0.dll %deployDir%
+xcopy %QtDir%libvorbis-0.dll %deployDir%
+xcopy %QtDir%libopus-0.dll %deployDir%
+xcopy %QtDir%libx264-142.dll %deployDir%
+xcopy %QtDir%libvorbisenc-2.dll %deployDir%
+xcopy %QtDir%libogg-0.dll %deployDir%
+xcopy %QtDir%Qt5OpenGL.dll %deployDir%
+xcopy %QtDir%Qt5Compress.dll %deployDir%
+xcopy .\src\rename_me_for_local_settings.ini %deployDir%
+xcopy .\src\XTPlayerDebug.bat %deployDir%
+xcopy .\src\README.txt %deployDir%
 
 pause
