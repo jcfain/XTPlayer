1bceed5d262fdafd44fdb971dce8d52f5be3b2d5
Jason C. Fain <jfain878@gmail.com>
2021-06-28 21:13:29 -0400

    Updateing build for 258

--- lin_release_deploy.sh
+++ lin_release_deploy.sh
@@ -4,10 +4,10 @@ cqtdeployer -confFile cqt-deb.conf
 #-libDir /home/jay/Qt/5.15.0/gcc_64/lib
 #-targetDir ./XTPlayer-release noOverwrite deploySystem extractPlugins
 #-deployVersion 0.13b -releaseDate $(date) +'%m/%d/%Y' -icon ./src/images/icons/XTP-icon.png
-cp -R src/themes ./XTPlayer-v0.255b_Linux_x86_64/bin
-cp -R lib/* ./XTPlayer-v0.255b_Linux_x86_64/lib
-cp src/rename_me_for_local_settings.ini ./XTPlayer-v0.255b_Linux_x86_64/bin
-cp "XTP and DeoVR guide.pdf" ./XTPlayer-v0.255b_Linux_x86_64
+cp -R src/themes ./XTPlayer-v0.258b_Linux_x86_64/bin
+cp -R lib/* ./XTPlayer-v0.258b_Linux_x86_64/lib
+cp src/rename_me_for_local_settings.ini ./XTPlayer-v0.258b_Linux_x86_64/bin
+cp "XTP and DeoVR guide.pdf" ./XTPlayer-v0.258b_Linux_x86_64
 read -p "Press enter to resume ..."
 
 
