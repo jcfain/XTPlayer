289208190ae9f8620b2f34d06e2cee3dd9ae21ea
Jason Fain <jfain878@gmail.com>
2022-01-31 17:20:12 -0500

    Up the version for release

--- src/lib/handler/settingshandler.cpp
+++ src/lib/handler/settingshandler.cpp
@@ -4,8 +4,8 @@ const QMap<TCodeVersion, QString> SettingsHandler::SupportedTCodeVersions = {
     {TCodeVersion::v2, "TCode v0.2"},
     {TCodeVersion::v3, "TCode v0.3"}
 };
-const QString SettingsHandler::XTPVersion = "0.282";
-const float SettingsHandler::XTPVersionNum = 0.282f;
+const QString SettingsHandler::XTPVersion = "0.283";
+const float SettingsHandler::XTPVersionNum = 0.283f;
 
 SettingsHandler::SettingsHandler(){}
 SettingsHandler::~SettingsHandler()
