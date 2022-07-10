44bf3f5f3c783398878e2eb9dd7d1733931408d7
Fain <25233821+jcfain@users.noreply.github.com>
2022-07-10 18:25:14 -0400

    Update lin_release_deploy.sh

--- lin_release_deploy.sh
+++ lin_release_deploy.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-targetDirectory=./Deploy/XTPlayer-v0.3a_Linux_x86_64
+targetDirectory=./Deploy/XTPlayer-v0.33a_Linux_x86_64
 binDirectory=./build-XTPlayer-Desktop_Qt_5_15_2_GCC_64bit-Release/XTPlayer
+xteBinDirectory=../XTEngine/build-XTEngine-Desktop_Qt_5_15_2_GCC_64bit-Release/release
 librDirectory=/home/jay/Qt/5.15.2/gcc_64/lib
 qmakeDirectory=/home/jay/Qt/5.15.2/gcc_64/bin/qmake
+cqtdeployer -bin $xteBinDirectory clear -libDir $librDirectory -qmake $qmakeDirectory -targetDir $targetDirectory
 cqtdeployer -bin $binDirectory clear -libDir $librDirectory -qmake $qmakeDirectory -targetDir $targetDirectory
 mkdir $targetDirectory/bin/themes
 cp -R src/themes/ $targetDirectory/bin/
 mkdir $targetDirectory/bin/www
-cp src/www/*-min.html $targetDirectory/bin/www/
-cp src/www/*-min.js $targetDirectory/bin/www/
-cp src/www/*-min.css $targetDirectory/bin/www/
-cp src/www/favicon.ico $targetDirectory/bin/www/
+cp $xteBinDirectory/www/*-min.html $targetDirectory/bin/www/
+cp $xteBinDirectory/www/*-min.js $targetDirectory/bin/www/
+cp $xteBinDirectory/www/*-min.css $targetDirectory/bin/www/
+cp $xteBinDirectory/www/favicon.ico $targetDirectory/bin/www/
 cp -R Deploy/lib/* $targetDirectory/lib
-cp src/rename_me_for_local_settings.ini $targetDirectory/bin
+cp ../XTEngine/src/rename_me_for_local_settings.ini $targetDirectory/bin
+cp $xteBinDirectory/lib* $targetDirectory/lib
 cp "XTP and DeoVR guide.pdf" $targetDirectory/"XTP and DeoVR guide.pdf"
 read -p "Press enter to resume ..."
 
