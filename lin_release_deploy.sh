41dc8cffb1ade6597a4a0b0ec8f25fdd397ac1d7
Fain <25233821+jcfain@users.noreply.github.com>
2022-07-16 08:09:52 -0400

    Fix issue with linux deploy

--- lin_release_deploy.sh
+++ lin_release_deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-targetDirectory=./Deploy/XTPlayer-v0.33a_Linux_x86_64
+version=v0.33a
+targetDirectory=./Deploy/XTPlayer-${version}_Linux_x86_64
+xteTargetDirectory=./Deploy/XTEngine-${version}_Linux_x86_64
 binDirectory=./build-XTPlayer-Desktop_Qt_5_15_2_GCC_64bit-Release/XTPlayer
 xteBinDirectory=../XTEngine/build-XTEngine-Desktop_Qt_5_15_2_GCC_64bit-Release/release
 librDirectory=/home/jay/Qt/5.15.2/gcc_64/lib
 qmakeDirectory=/home/jay/Qt/5.15.2/gcc_64/bin/qmake
-cqtdeployer -bin $xteBinDirectory clear -libDir $librDirectory -qmake $qmakeDirectory -targetDir $targetDirectory
+cqtdeployer -bin $xteBinDirectory clear -libDir $librDirectory -qmake $qmakeDirectory -targetDir $xteTargetDirectory
 cqtdeployer -bin $binDirectory clear -libDir $librDirectory -qmake $qmakeDirectory -targetDir $targetDirectory
 mkdir $targetDirectory/bin/themes
 cp -R src/themes/ $targetDirectory/bin/
@@ -15,8 +17,9 @@ cp $xteBinDirectory/www/*-min.js $targetDirectory/bin/www/
 cp $xteBinDirectory/www/*-min.css $targetDirectory/bin/www/
 cp $xteBinDirectory/www/favicon.ico $targetDirectory/bin/www/
 cp -R Deploy/lib/* $targetDirectory/lib
+cp -R Deploy/XTEngine-v0.33a_Linux_x86_64/lib/* $targetDirectory/lib
 cp ../XTEngine/src/rename_me_for_local_settings.ini $targetDirectory/bin
-cp $xteBinDirectory/lib* $targetDirectory/lib
+cp $xteBinDirectory/*.* $targetDirectory/lib
 cp "XTP and DeoVR guide.pdf" $targetDirectory/"XTP and DeoVR guide.pdf"
 read -p "Press enter to resume ..."
 
