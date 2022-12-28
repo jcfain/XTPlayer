6695dd4fc67926dfa9af794337370e1445119475
Fain <25233821+jcfain@users.noreply.github.com>
2022-12-28 11:50:24 -0500

    Update appimage deploy script

---
+++ lin_release_deploy-appimage.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Enter version: (ex: 0.423b)"  
+read versionInput
+export VERSION=v${versionInput}
+
+home=~/
+target='/media/NAS/STK/Hardware/my software/'
+xtplayerSource="${home}"git/XTPlayer
+xtplayerLocation="${home}"git/XTPlayer/build-XTPlayer-Desktop_Qt_5_15_2_GCC_64bit-Release/
+xtengineLocation="${home}"git/XTEngine/build-XTEngine-Desktop_Qt_5_15_2_GCC_64bit-Release/release/
+httpserverLocation="${home}"git/HttpServer/src/build/release/
+deployDirectory="${home}"git/XTPlayer/Deploy/
+linuxdeployqtBinary="${home}git/linuxdeployqt-continuous-x86_64.AppImage"
+qtDirectory="${home}"Qt/5.15.2/gcc_64/bin
+
+#Export directory with qmake to path.
+export PATH="${qtDirectory}":$PATH
+export LD_LIBRARY_PATH="${deployDirectory}lib"
+
+#echo LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}
+#echo PATH: ${PATH}
+#echo Home: ${home}
+#echo Target: ${target}
+
+mkdir -p "${deployDirectory}lib"
+find "${xtengineLocation}" \( -name "moc_*" -or -name "*.o" -or -name "qrc_*" -or -name "Makefile*" -or -name "*.a" \) -exec rm {} \;
+cp -r "${xtengineLocation}"* "${deployDirectory}lib"
+cp -r "${httpserverLocation}"* "${deployDirectory}lib"
+find "${xtplayerLocation}" \( -name "moc_*" -or -name "*.o" -or -name "qrc_*" -or -name "Makefile*" -or -name "*.a" \) -exec rm {} \;
+cp "${xtplayerSource}"/XTPlayer.desktop "${xtplayerLocation}"/XTPlayer.desktop
+
+cp "${xtplayerSource}"/src/images/icons/XTP-icon.png ${xtplayerLocation}/XTPlayer.png
+"${linuxdeployqtBinary}" ${xtplayerLocation}/XTPlayer -appimage -unsupported-allow-new-glibc -always-overwrite -extra-plugins=iconengines
+#,platformthemes/libqgtk3.so
+#mv ${home}git/XTPlayer.appimage  ${home}git/XTPlayer-v$(version).appimage
+mv "${xtplayerSource}"/XTPlayer-"${VERSION}"-x86_64.AppImage "${deployDirectory}"XTPlayer-"${VERSION}"-Linux-x86_64.AppImage
+cp "${deployDirectory}"XTPlayer-"${VERSION}"-Linux-x86_64.AppImage "${target}"XTPlayer-"${VERSION}"-Linux-x86_64.AppImage
