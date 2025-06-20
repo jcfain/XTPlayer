169238ecbc0576d179118ac82cf52da6ea45aabf
Fain <25233821+jcfain@users.noreply.github.com>
2025-06-20 12:17:28 -0400

    Update mac deploy for Qt 6

--- mac_release_deploy.sh
+++ mac_release_deploy.sh
@@ -1,22 +1,17 @@
-cd build-XTPlayer-Desktop_Qt_5_15_2_clang_64bit-Release
-~/Qt/5.15.2/clang_64/bin/macdeployqt XTPlayer.app
-cp -R ../src/themes XTPlayer.app/Contents/MacOS/
-cp -R ../src/images XTPlayer.app/Contents/MacOS/
-mkDir XTPlayer.app/Contents/MacOS/www
-cp ../../XTEngine/src/www/*-min.html XTPlayer.app/Contents/MacOS/www/
-cp ../../XTEngine/src/www/*-min.js XTPlayer.app/Contents/MacOS/www/
-cp ../../XTEngine/src/www/*-min.css XTPlayer.app/Contents/MacOS/www/
-cp ../../XTEngine/src/www/favicon.ico XTPlayer.app/Contents/MacOS/www/favicon.ico
-cp ../../HttpServer/src/build/release/libhttpServer.1.0.0.dylib XTPlayer.app/Contents/Frameworks/
-cp ../../HttpServer/src/build/release/libhttpServer.1.0.dylib XTPlayer.app/Contents/Frameworks/
-cp ../../HttpServer/src/build/release/libhttpServer.1.dylib XTPlayer.app/Contents/Frameworks/
-cp ../../HttpServer/src/build/release/libhttpServer.dylib XTPlayer.app/Contents/Frameworks/
-cp ../../XTEngine/build-XTEngine-Desktop_Qt_5_15_2_clang_64bit-Release/release/lib* XTPlayer.app/Contents/Frameworks/
-install_name_tool -change libhttpServer.1.dylib @executable_path/../Frameworks/libhttpServer.1.dylib XTPlayer.app/Contents/MacOS/XTPlayer
-install_name_tool -change libhttpServer.1.dylib @executable_path/../Frameworks/libhttpServer.1.dylib XTPlayer.app/Contents/Frameworks/libxtengine.1.0.0.dylib
-install_name_tool -change libxtengine.1.0.0.dylib @executable_path/../Frameworks/libxtengine.1.0.0.dylib XTPlayer.app/Contents/MacOS/XTPlayer
-version="v0.45b"
+cd build-release
+appLocation="release/XTPlayer.app"
+~/Qt/6.9.1/macos/bin/macdeployqt ${appLocation}
+cp -R ../src/themes ${appLocation}/Contents/MacOS/
+cp -R ../src/images ${appLocation}/Contents/MacOS/
+mkDir ${appLocation}/Contents/MacOS/www
+cp ../../XTEngine/src/www/*-min.html ${appLocation}/Contents/MacOS/www/
+cp ../../XTEngine/src/www/*-min.js ${appLocation}/Contents/MacOS/www/
+cp ../../XTEngine/src/www/*-min.css ${appLocation}/Contents/MacOS/www/
+cp ../../XTEngine/src/www/favicon.ico ${appLocation}/Contents/MacOS/www/favicon.ico
+cp ../../XTEngine/build-release/release/lib* ${appLocation}/Contents/Frameworks/
+install_name_tool -change libxtengine.1.0.0.dylib @executable_path/../Frameworks/libxtengine.1.0.0.dylib ${appLocation}/Contents/MacOS/XTPlayer
+version="v0.5b"
 mkdir XTPlayer-${version}_MacOS_x86_64
-cp -R XTPlayer.app XTPlayer-${version}_MacOS_x86_64/
+cp -R ${appLocation} XTPlayer-${version}_MacOS_x86_64/
 hdiutil create /tmp/tmp.dmg -ov -volname "XTPlayer-${version}_MacOS_x86_64" -fs HFS+ -srcfolder "XTPlayer-${version}_MacOS_x86_64"
 hdiutil convert /tmp/tmp.dmg -format UDZO -o XTPlayer-${version}_MacOS_x86_64.dmg
