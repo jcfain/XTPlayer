556b395785b2beb276707fb1c462dce092d4be1e
Fain <25233821+jcfain@users.noreply.github.com>
2025-07-20 12:25:06 -0400

    Update mac deploy script for easier use

--- mac_release_deploy.sh
+++ mac_release_deploy.sh
@@ -10,8 +10,9 @@ cp ../../XTEngine/src/www/*-min.css ${appLocation}/Contents/MacOS/www/
 cp ../../XTEngine/src/www/favicon.ico ${appLocation}/Contents/MacOS/www/favicon.ico
 cp ../../XTEngine/build-release/release/lib* ${appLocation}/Contents/Frameworks/
 install_name_tool -change libxtengine.1.0.0.dylib @executable_path/../Frameworks/libxtengine.1.0.0.dylib ${appLocation}/Contents/MacOS/XTPlayer
-version="v0.5b"
-mkdir XTPlayer-${version}_MacOS_x86_64
-cp -R ${appLocation} XTPlayer-${version}_MacOS_x86_64/
-hdiutil create /tmp/tmp.dmg -ov -volname "XTPlayer-${version}_MacOS_x86_64" -fs HFS+ -srcfolder "XTPlayer-${version}_MacOS_x86_64"
-hdiutil convert /tmp/tmp.dmg -format UDZO -o XTPlayer-${version}_MacOS_x86_64.dmg
+version="v0.54b"
+outname="XTPlayer-${version}_MacOS_Universal"
+mkdir ${outname}
+cp -R ${appLocation} ${outname}/
+hdiutil create /tmp/tmp.dmg -ov -volname ${outname} -fs HFS+ -srcfolder ${outname}
+hdiutil convert /tmp/tmp.dmg -format UDZO -o ${outname}.dmg
