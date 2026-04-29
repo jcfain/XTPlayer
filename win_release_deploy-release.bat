b362873e9c0ddc803628047b5e8f26855a803b96
Fain <25233821+jcfain@users.noreply.github.com>
2026-04-28 21:59:48 -0400

    build batch for 6.11

--- win_release_deploy-release.bat
+++ win_release_deploy-release.bat
@@ -1,7 +1,7 @@
 @echo off
 
 SET currentPath=%cd%
-SET QTDIR=C:\Qt\6.9.1\mingw_64
+SET QTDIR=C:\Qt\6.11.0\mingw_64
 SET mingwDir=C:\Qt\Tools\mingw1310_64\bin
 SET QtDirBin=%QTDIR%\bin\
 SET xtplayerSource="%UserProfile%\git\XTPlayer"
@@ -16,7 +16,7 @@ SET stagingDirectory=%xtplayerSource%\bin\latest
 SET sevenZipLocation="C:/Program Files/7-Zip/7z.exe"
 SET mingw=%mingwDir%\mingw32-make.exe
 rem this is where the zip file will be copied for backup
-SET releaseDirectory="\\192.168.0.218\main\STK\Dev\XTP\XTP-Release\"
+SET releaseDirectory="\\192.168.0.201\main\STK\Dev\XTP\XTP-Release\"
 rem SET httpServerDll=..\HttpServer\build\release\httpServer.dll
 rem SET zlibDll=..\zlib-1.3.1\build\Desktop_Qt_5_15_2_MinGW_64_bit-Release\libzlib.dll
 
